72a3861a5a45383f8f47fa71fb905a8960da256c
osamaelfouly <42895946+osamaelfouly@users.noreply.github.com>
2018-10-16 23:30:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,7 +24,6 @@ const prefix = "!";
 /////////////////////////
 ////////////////////////
 
-const prefix = "!"
 client.on('message', async msg => { 
 	if (msg.author.bot) return undefined;
 	if (!msg.content.startsWith(prefix)) return undefined;
