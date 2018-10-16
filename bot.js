9ce52dffb23e5234cdda238a32a11ab7c1ae9558
osamaelfouly <42895946+osamaelfouly@users.noreply.github.com>
2018-10-16 23:37:43 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,6 +20,8 @@ const gif = require("gif-search");
 
 const client = new Discord.Client({disableEveryone: true});
 
+var servers = {};
+
 const prefix = "!";
 /////////////////////////
 ////////////////////////
@@ -209,4 +211,23 @@ function play(guild, song) {
 
 	serverQueue.textChannel.send(`بدء تشغيل : **${song.title}**`);
 }
+
+function play(connection, message, args) {
+  var server = servers[message.guild.id];
+  server.dispatcher = connection.playStream(YTDL(args[0]), {filter: "audioonly"});
+  server.queue.shift();
+  server.dispatcher.on("end", function() {
+    if (server.queue[0]) play(connection, message);
+    else connection.disconnect();
+  });
+}
+
+
+client.on('message', message =>{
+  if(message.content.startsWith('!join')){
+    const voiceChannel = message.member.voiceChannel
+    voiceChannel.join();
+    message.channel.send("تم الأتصال بالروم الصوتي")
+}})
+
 client.login(process.env.BOT_TOKEN);
