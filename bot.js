5bb9250f25c3fa2e77ba1a4d7da48b47757c51a8
osamaelfouly <42895946+osamaelfouly@users.noreply.github.com>
2018-10-18 12:35:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -57,15 +57,7 @@ if (message.content.startsWith(adminprefix + 'setT')) {
 
 
 
-client.on('message', async msg => { 
-	if (msg.author.bot) return undefined;
-	if (!msg.content.startsWith(prefix)) return undefined;
-	const args = msg.content.split(' ');
-	const searchString = args.slice(1).join(' ');
-	const url = args[1] ? args[1].replace(/<(.+)>/g, '$1') : '';
-	const serverQueue = queue.get(msg.guild.id);
-	let command = msg.content.toLowerCase().split(" ")[0];
-	command = command.slice(prefix.length)
+
 	if (command === `play`) {
 		const voiceChannel = msg.member.voiceChannel;
 		if (!voiceChannel) return msg.channel.send('يجب توآجد حضرتك بروم صوتي .');
